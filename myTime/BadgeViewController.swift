ed506a4e35ba832735ca89dddcd0aab4e3b4a5cc
Luigi Cerrato <luigi-cerrato@hotmail.it>
2019-12-05 11:37:50 +0100

    Added Data and Time at first screen

--- myTime/BadgeViewController.swift
+++ myTime/BadgeViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            getTime()
+        
+    }
+    
+    func getTime(){
+        dateLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .long, timeStyle: .none)
+            timeLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .short)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.tick) , userInfo: nil, repeats: true)
+        
     }
 
+    @objc func tick() {
+        dateLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .long, timeStyle: .none)
+        timeLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .short)
+    }
 
 }
+
 
