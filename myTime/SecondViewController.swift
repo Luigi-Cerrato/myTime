45a3244d1ea9ad1d630883849a2a7334e9e8ff20
Luigi Cerrato <luigi-cerrato@hotmail.it>
2019-12-05 08:38:09 +0100

    Initial Commit

---
+++ myTime/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  myTime
+//
+//  Created by Luigi Cerrato on 05/12/2019.
+//  Copyright © 2019 Luigi Cerrato. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
